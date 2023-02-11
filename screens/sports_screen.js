ff69eb9fde8fe478a26a36f33c8a85f58a05ce4b
Sendiko <sarangtawon897@gmail.com>
2023-02-11 10:55:55 +0700

    enhancement - create sports screen

---
+++ screens/sports_screen.js
@@ -0,0 +1,22 @@
+
+import { View, StyleSheet, Text, Button } from "react-native"
+
+const SportsScreen = () => {
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+export default SportsScreen
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#9D0000",
+      flexDirection: "column",
+      justifyContent: "center",
+      verticalAlign: "center"
+    },
+})
