c73f0f35c84b5ded3e79e6219c19343b836abdae
Sendiko <sarangtawon897@gmail.com>
2023-02-11 11:37:57 +0700

    refactor - code reformat

--- components/custom_button.js
+++ components/custom_button.js
@@ -2,26 +2,26 @@ import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
 const PrimaryButton = (props) => {
-    return(
-        <TouchableOpacity style={style.primaryButton} onPress={props.onPress}>
-            <Text style={style.onPrimaryButton}>{props.text}</Text>
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity style={style.primaryButton} onPress={props.onPress}>
+      <Text style={style.onPrimaryButton}>{props.text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const style = StyleSheet.create({
-    primaryButton: {
-        backgroundColor: '#B10E0E',
-        borderRadius: 100,
-        width: '80%'
-    },
-    onPrimaryButton: {
-        color: '#fbfaff',
-        margin: 16,
-        textAlign: 'center',
-        fontSize: 16,
-        fontWeight: '600'
-    }
-})
+  primaryButton: {
+    backgroundColor: "#B10E0E",
+    borderRadius: 100,
+    width: "80%",
+  },
+  onPrimaryButton: {
+    color: "#fbfaff",
+    margin: 16,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
 
-export default PrimaryButton
+export default PrimaryButton;
